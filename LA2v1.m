9d4307924629807b5e34ccd7f098b0b0e2a2c86e
Vince <Vince@DESKTOP-QQ2U72F>
2023-10-12 16:09:37 +1100

    commit

--- LA2v1.m
+++ LA2v1.m
@@ -17,12 +17,17 @@ surf([-3,-3;3,3] ...
     ,'CData',imread('concrete.jpg') ...
     ,'FaceColor','texturemap');
 
+%setup environment
+
+%long table, chairs, cooktop, grill
+table = PlaceObject('barrier1.5x0.2x1m.ply',[0,1,0]);
+verts = [get(fence1,'Vertices'), ones(size(get(fence1,'Vertices'),1),1)]* trotz(pi/2);
+verts(:,2) = verts(:,2)*2;
+set(fence1,'Vertices',verts(:,1:3));
+
 %setup dobot magician
 r = DobotMagician(true);
 
 % %setup created robot
 r2baseTr = baseTr*transl(0.5,0,0.2);
 r2 = DVince(r2baseTr);
-
-
-
